d6fd3427a3ca9ee73710f9f1a071262a7e69fa86
Sarah Smith <sarah@smithsoft.com.au>
2015-02-01 01:27:16 +1000

    Create initial plugin stub.
    Has no content but can drag a node into a scene.

---
+++ SpriteBuilder/CCStateMachine/CCBPStateMachine.h
@@ -0,0 +1,15 @@
+//
+//  CCBPSprite.h
+//  SpriteBuilder
+//
+//  Created by Viktor on 12/17/13.
+//
+//
+
+#import "CCSprite.h"
+#import "EffectsManager.h"
+
+@interface CCBPStateMachine : CCNode
+
+
+@end
---
+++ SpriteBuilder/CCStateMachine/CCBPStateMachine.m
@@ -0,0 +1,18 @@
+//
+//  CCBPSprite.m
+//  SpriteBuilder
+//
+//  Created by Viktor on 12/17/13.
+//
+//
+
+#import "CCBPStateMachine.h"
+#import "NSArray+Query.h"
+#import "CCEffect.h"
+#import "CCEffectStack.h"
+#import "AppDelegate.h"
+#import "CCBDocument.h"
+
+@implementation CCBPStateMachine
+
+@end
